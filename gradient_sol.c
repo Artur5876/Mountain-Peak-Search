cccfceb703adf1b866a5d1f21166f796075c9a9d
arturromanov75@gmail.com <arturromanov75@gmail.com>
2026-04-17 12:55:06 +0100

    Make step size proportional to gradient strength

--- gradient_sol.c
+++ gradient_sol.c
@@ -1,4 +1,5 @@
 #include "gradient.h"
+#include <math.h>
 
 static void get_view(int y, int x, float view[VIEW_SIZE][VIEW_SIZE]) {
     generate_view(view, y, x);
@@ -33,35 +34,64 @@ static int is_local_peak(const float view[VIEW_SIZE][VIEW_SIZE]) {
     return 1;
 }
 
+static void compute_gradient(const float view[VIEW_SIZE][VIEW_SIZE], 
+                            float *gx, float *gy) {
+    int half = VIEW_SIZE / 2;
+    float left = 0, right = 0, up = 0,down = 0;
+    int lc = 0, rc = 0, uc = 0, dc = 0;
+    for (int i =0; i < VIEW_SIZE; ++i) {
+        for (int j =0; j < VIEW_SIZE; j++) {
+            float v = view[i][j];
+            if (v < 0.0f) continue;
+
+            if (j > half) {right += v; ++lc;}
+            else if (j < half) {left += v; ++rc;}
+
+            if (i < half) {up += v; ++uc;}
+            else if (i > half) {down += v; ++dc;}
+        }
+    }
+    *gx = (rc ? right/rc : 0) - (lc ? left/lc : 0);
+    *gy = (dc ? down/dc : 0) - (up ? up/uc : 0);
+}
 path_point find_highest_point(void) {
     int x = 0, y = 0;
     float view[VIEW_SIZE][VIEW_SIZE];
     int max_iterations = 1000;   // safeguard
     int iter = 0;
+    const int STEP = VIEW_SIZE /2;
+
 
     while (iter < max_iterations) {
         iter++;
         get_view(y, x, view);
+
         if (is_local_peak(view)) {
             if (declare_peak(x, y)) {
                 path_point p = {x, y};
                 return p;
             }
         }
-        int max_dy, max_dx;
-        view_max_pos(view, &max_dy, &max_dx);
-        int new_x = x + max_dx - VIEW_SIZE/2;
-        int new_y = y + max_dy - VIEW_SIZE/2;
 
-        //if there is no progress, then break
-        if (new_x == x && new_y == y) {
-            break;
+        float gx, gy;
+        compute_gradient(view, &gx, &gy);
+
+        if (fabs(gx) > 0.01f || fabs(gy) > 0.01f) {
+            float len = sqrt(gx*gx + gy*gy);
+
+            int dx = (int)(STEP * gx / len);
+            int dy = (int)(STEP * gy / len);
+            x += dx;
+            y += dy;
+        } else {
+            int max_dy, max_dx;
+            view_max_pos(view, &max_dy, &max_dx);
+            x += max_dx - VIEW_SIZE/2;
+            y += max_dy - VIEW_SIZE/2;
         }
-        x = new_x;
-        y = new_y;
+
+        //keep coordinates non-negative;
+        if (x < 0) x=0;
+        if(y < 0) y =0;
     }
-    //declare whatever we have (might be wrong)
-    declare_peak(x, y);
-    path_point p = {x, y};
-    return p;
 }
